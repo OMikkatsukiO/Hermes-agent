271c00276dfe3cefa4f8a8f638e34e5916935ab3
OMikkatsukiO <zzz5657889@gmail.com>
2026-07-07 11:56:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -532,4 +532,4 @@ with gr.Blocks(
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7860))
-    demo.launch(server_name="0.0.0.0", server_port=port)
+demo.launch(server_name="0.0.0.0", server_port=port)
